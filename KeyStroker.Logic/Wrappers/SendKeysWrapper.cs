52b3a888ff22b4d329660f5827fa60bde85679f1
mjuhola42 <juholamatias7@gmail.com>
2021-01-26 01:12:14 +0200

    Prototyping of the Key class and wrapper for keysending

--- KeyStroker.Logic/Wrappers/SendKeysWrapper.cs
+++ KeyStroker.Logic/Wrappers/SendKeysWrapper.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace KeyStroker.Logic
 {
+    // Static Wrapper Class for all keysending
+    // for future check SendInput from user32.dll
     public static class SendKeysWrapper
     {
         
         public static void SendSingleKey(char key)
         {
-            
+            SendKeys.Send(key.ToString());
         }
 
         public static void SendMultipleKeys(string keys)
         {
-
+            SendKeys.Send(keys);
         }
     }
 }
