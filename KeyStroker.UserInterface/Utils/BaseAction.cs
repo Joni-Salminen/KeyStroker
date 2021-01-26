7ac3e6c5307b80b4910bc56b8649fa7e3441d5d9
Joni Salminen <joni.k.salminen@gmail.com>
2021-01-26 18:23:05 +0200

    Control utility tools

---
+++ KeyStroker.UserInterface/Utils/BaseAction.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace KeyStroker.UI.Utils
+{
+
+    public class BaseAction : ICommand
+    {
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        private Action methodToExecute;
+        private Func<bool> canExecuteEvaluator;
+        public BaseAction(Action methodToExecute, Func<bool> canExecuteEvaluator)
+        {
+            this.methodToExecute = methodToExecute;
+            this.canExecuteEvaluator = canExecuteEvaluator;
+        }
+
+        public BaseAction(Action methodToExecute)
+            : this(methodToExecute, null) { }
+
+        public bool CanExecute(object parameter)
+        {
+            if (this.canExecuteEvaluator == null)
+            {
+                return true;
+            }
+            else
+            {
+                bool result = this.canExecuteEvaluator.Invoke();
+                return result;
+            }
+        }
+        public void Execute(object parameter)
+        {
+            this.methodToExecute.Invoke();
+        }
+    }
+}
+
---
+++ KeyStroker.UserInterface/Utils/BindingProxy.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace KeyStroker.UI.Utils
+{
+    public class BindingProxy : Freezable
+    {
+        #region Overrides of Freezable
+
+        protected override Freezable CreateInstanceCore()
+        {
+            return new BindingProxy();
+        }
+
+        #endregion
+
+        public object DataContext
+        {
+            get { return (object)GetValue(DataProperty); }
+            set { SetValue(DataProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Data.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DataProperty =
+            DependencyProperty.Register("DataContext", typeof(object), typeof(BindingProxy), new UIPropertyMetadata(null));
+    }
+}
