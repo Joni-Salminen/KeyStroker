cf07a415b54f400f4f059dcf6ef07e438e27116d
mjuhola42 <53136872+mjuhola42@users.noreply.github.com>
2021-01-26 21:09:20 +0200

    Created KeyController class which is responsible for Key objects

---
+++ KeyStroker.Logic/KeyController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyStroker.Logic
+{
+    public class KeyController
+    {
+        public List<Key> keyList = new List<Key>();
+        private bool _isTimersRunning = false;
+
+        public bool isTimersRunning() => _isTimersRunning;
+        public bool registerNewKey(char key, double interval)
+        {
+            if (interval <= 5)
+                return false;
+            
+            keyList.Add(new Key(key, interval));
+            return true;
+        }
+        public void startAllTimers()
+        {
+            foreach(Key key in keyList)
+            {
+                key.StartTimer();
+            }
+            _isTimersRunning = true;
+        }
+        public void stopAllTimers()
+        {
+            foreach(Key key in keyList)
+            {
+                key.StopTimer();
+            }
+            _isTimersRunning = false;
+        }
+    }
+}
