63ddef028e8483117a53a92494f3d458fb33de98
Joni Salminen <joni.k.salminen@gmail.com>
2021-01-28 18:53:06 +0200

    initial views

---
+++ KeyStroker.UserInterface/Viewmodels/KeySpammerViewmodel.cs
@@ -0,0 +1,36 @@
+﻿using KeyStroker.UI.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeyStroker.UI.Viewmodels
+{
+    public class KeySpammerViewmodel : BaseViewmodel {
+
+
+        private BaseAction _set;
+
+        public BaseAction SetKey {
+            get {
+                _set = new BaseAction(SetKey);
+                    return this._set; 
+            }
+            set { _set = value; }
+        }
+
+
+        /* Public empty constructor */
+        public KeySpammerViewmodel() {
+
+        }
+
+
+        public void SetKey() {
+            /* Call function to grab next key pressed */   
+
+
+
+        }
+
+    }
+}
