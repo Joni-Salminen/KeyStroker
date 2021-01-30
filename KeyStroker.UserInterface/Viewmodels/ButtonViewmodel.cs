021013c28c23ad267620e5d248b2af33faa5ae4f
Joni Salminen <joni.k.salminen@gmail.com>
2021-01-30 12:46:05 +0200

    Created button viewmodel
    This is the UI representation of Programmable Keys/actions

---
+++ KeyStroker.UserInterface/Viewmodels/ButtonViewmodel.cs
@@ -0,0 +1,43 @@
+﻿using KeyStroker.UI.Utils;
+using System.Windows.Input;
+
+namespace KeyStroker.UI.Viewmodels {
+
+    /* Represents UserDefined Key to UI */
+    public class ButtonViewmodel : BaseViewmodel {
+
+        private Key key;
+        private bool infiniteTimes = true;
+        private bool enabled = true;
+        private long repeatAmount = 0;
+        private long interval = 100;
+
+        /* The Button we are focusing on */
+        public Key KeyCode {
+            get => key;
+            set {
+                key = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        public long Interval { get => interval; set { interval = value; NotifyPropertyChanged(); } }
+        public bool IsEnabled { get => enabled; set { enabled = value; NotifyPropertyChanged(); } }
+        public bool InfinityTimes { get => infiniteTimes; set { infiniteTimes = value; NotifyPropertyChanged(); } }
+        public long RepeatAmount { get => repeatAmount; set { repeatAmount = value; NotifyPropertyChanged(); } }
+
+        public ButtonViewmodel() {}
+
+        public ButtonViewmodel(Key keyCode, bool enabled, long repeatAmount) {
+            this.KeyCode = keyCode;
+            this.IsEnabled = enabled;
+            this.RepeatAmount = repeatAmount;
+            if (RepeatAmount == 0) {
+                InfinityTimes = true;
+            }
+        }
+
+
+    }
+}
+
