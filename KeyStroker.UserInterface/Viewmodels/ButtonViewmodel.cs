da385b855084e734a54250e4aaf1b1a1ba0990ef
Joni Salminen <joni.k.salminen@gmail.com>
2021-01-30 21:37:56 +0200

    Small fixes

--- KeyStroker.UserInterface/Viewmodels/ButtonViewmodel.cs
+++ KeyStroker.UserInterface/Viewmodels/ButtonViewmodel.cs
@@ -7,20 +7,23 @@ namespace KeyStroker.UI.Viewmodels {
     /* Represents UserDefined Key to UI */
     public class ButtonViewmodel : BaseViewmodel {
 
-        private ProgrammableButton btn = new ProgrammableButton();
+        private readonly ProgrammableButton btn = new ProgrammableButton();
         
-        /* The Button we are focusing on */
-
+        /* UI only properties */
         public string KeyCodeStr { get => btn.KeyCode.ToString(); set { } }
+        public bool InfinityTimes { get => (btn.RepeatAmount == 0); set { NotifyPropertyChanged(); } }
 
-        public Key KeyCode {  get => btn.KeyCode; set { btn.KeyCode = value; NotifyPropertyChanged(); } }
-        public long Interval { get => btn.Interval; set { btn.Interval = value; NotifyPropertyChanged(); } }
-        public bool IsEnabled { get => btn.IsEnabled; set { btn.IsEnabled = value; NotifyPropertyChanged(); } }
-        public bool InfinityTimes { get => (btn.RepeatAmount == 0 ? true : false); set { NotifyPropertyChanged(); } }
-        public long RepeatAmount { get => btn.RepeatAmount; set { btn.RepeatAmount = value; NotifyPropertyChanged(); } }
+
+        public Key KeyCode {  get => btn.KeyCode; set { btn.KeyCode = value; NotifyPropertyChanged(); NotifyPropertyChanged("KeyCodeStr"); } }
+        public long Interval { get => btn.Interval; set { btn.Interval = value; NotifyPropertyChanged();  } }
+        public bool IsEnabled { get => btn.IsEnabled; set { btn.IsEnabled = value; NotifyPropertyChanged(); } }      
+        public long RepeatAmount { get => btn.RepeatAmount; set { btn.RepeatAmount = value; NotifyPropertyChanged(); NotifyPropertyChanged("InfinityTimes"); } }
+
 
         public ButtonViewmodel() {}
+        public ButtonViewmodel(ProgrammableButton button) { btn = button; }
 
+        /* TO-DO remove in future, just for testing purposes before core is finished */
         public ButtonViewmodel(Key keyCode, bool enabled, long interval, long repeatAmount) {
             this.KeyCode = keyCode;
             this.IsEnabled = enabled;
@@ -31,6 +34,7 @@ namespace KeyStroker.UI.Viewmodels {
             }
         }
 
+       
 
     }
 }
