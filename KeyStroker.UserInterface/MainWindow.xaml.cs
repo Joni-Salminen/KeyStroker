4b6faff466b5286ef416b8431a4c8a9233704626
Joni Salminen <joni.k.salminen@gmail.com>
2021-01-30 12:43:49 +0200

    Added datacontext init

--- KeyStroker.UserInterface/MainWindow.xaml.cs
+++ KeyStroker.UserInterface/MainWindow.xaml.cs
@@ -1,15 +1,18 @@
-﻿using MahApps.Metro.Controls;
+﻿using KeyStroker.UI.Viewmodels;
+using MahApps.Metro.Controls;
 
 namespace KeyStroker.UI {
- 
+
     public partial class MainWindow : MetroWindow {
+
+        RootViewmodel model = new RootViewmodel();
         public MainWindow() {
             InitializeComponent();
             HamburgerMenu.SelectedIndex = 0;
+            DataContext = model;
         }
 
-        private void HamburgerMenu_ItemClick(object sender, MahApps.Metro.Controls.ItemClickEventArgs e)
-        {
+        private void HamburgerMenu_ItemClick(object sender, MahApps.Metro.Controls.ItemClickEventArgs e) {
             // set the content
             this.HamburgerMenu.Content = e.ClickedItem;
             // close the pane
