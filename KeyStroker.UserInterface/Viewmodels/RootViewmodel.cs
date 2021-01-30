315b9bf6ac5e98305e86effaeee23e382c4b5278
Joni Salminen <joni.k.salminen@gmail.com>
2021-01-30 12:44:50 +0200

    Created root viewmodel to control others
    This is basically the UI entry point

---
+++ KeyStroker.UserInterface/Viewmodels/RootViewmodel.cs
@@ -0,0 +1,20 @@
+﻿using KeyStroker.UI.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeyStroker.UI.Viewmodels {
+    public class RootViewmodel : BaseViewmodel {
+
+        private ButtonSpammerViewmodel _buttonSpammerVm;
+        public ButtonSpammerViewmodel ButtonSpammerVM { get => _buttonSpammerVm; set { _buttonSpammerVm = value; NotifyPropertyChanged(); }}
+
+
+        public RootViewmodel() {
+            ButtonSpammerVM = new ButtonSpammerViewmodel(); 
+
+
+
+        }
+    }
+}
