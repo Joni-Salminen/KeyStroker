a928d723e4f227cc08c4a810b970d83b6f433abf
mjuhola42 <53136872+mjuhola42@users.noreply.github.com>
2021-02-02 17:41:36 +0200

    Background listener prototyping

--- KeyStroker.UserInterface/MainWindow.xaml.cs
+++ KeyStroker.UserInterface/MainWindow.xaml.cs
@@ -1,25 +1,35 @@
 ﻿using KeyStroker.UI.Viewmodels;
 using MahApps.Metro.Controls;
 using MahApps.Metro.Controls.Dialogs;
+using System.Windows;
+using System.Windows.Interop;
+using KeyStroker.Logic;
 
 namespace KeyStroker.UI {
 
     public partial class MainWindow : MetroWindow {
 
         RootViewmodel vm;
+        BackgroundKeyListener listener;
         public MainWindow() {
             InitializeComponent();
             HamburgerMenu.SelectedIndex = 0;
             vm = new RootViewmodel(DialogCoordinator.Instance);
             DataContext = vm;
+
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            listener = new BackgroundKeyListener(new WindowInteropHelper(this).Handle);
         }
 
         private void HamburgerMenu_ItemClick(object sender, MahApps.Metro.Controls.ItemClickEventArgs e) {
             // set the content
             this.HamburgerMenu.Content = e.ClickedItem;
             // close the pane
-            this.HamburgerMenu.IsPaneOpen = false;
-           
+            this.HamburgerMenu.IsPaneOpen = false;  
         }
 
         private void OnKeyDown(object sender, System.Windows.Input.KeyEventArgs e) {
