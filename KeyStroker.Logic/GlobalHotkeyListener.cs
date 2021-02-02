db51468e29fcebfb716fa818bfd533f29bef5ce1
Joni Salminen <joni.k.salminen@gmail.com>
2021-02-02 18:34:30 +0200

    added GlobalHotkeyListener

---
+++ KeyStroker.Logic/GlobalHotkeyListener.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+
+namespace KeyStroker.Logic {
+    public sealed class GlobalHotkeyListener {
+
+        public event HotkeyPressed OnHotkeyPressed;
+
+        private static readonly Lazy<GlobalHotkeyListener> instance = new Lazy<GlobalHotkeyListener>(() => new GlobalHotkeyListener());
+        public static GlobalHotkeyListener Instance { get { return instance.Value; } }
+
+        private GlobalHotkeyListener() {
+            if (handle == null)
+                handle = Process.GetCurrentProcess().MainWindowHandle;
+        }
+
+        private IntPtr WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled) {
+            switch (msg) {
+                case WM_HOTKEY: {
+                        System.Windows.MessageBox.Show("");
+                        OnHotkeyPressed?.Invoke();
+                        break;
+                    }
+            }
+
+            return IntPtr.Zero;
+        }
+
+        private const int WM_HOTKEY = 0x312;
+        IntPtr handle;
+        HwndSource source;
+
+        #region DLLIMPORTS
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+        #endregion
+
+        /// <summary>
+        /// Register new Hotkey
+        /// </summary>
+        /// <param name="id">Identifier of the Hotkey</param>
+        /// <param name="modifiers">Keys that must be pressed in combination with the key</param>
+        /// <param name="key"> Virtual-key code of the hotkey </param>
+        /// <returns></returns>
+        public bool RegisterHotKey(int id, uint modifiers, uint key) {
+            return RegisterHotKey(source.Handle, id, modifiers, key);
+        }
+
+        /// <summary>
+        /// Remove registered hotkey
+        /// </summary>
+        /// <param name="id">Identifier to be removed</param>
+        /// <returns></returns>
+        public bool UnregisterHotKey(int id) {
+            return UnregisterHotKey(source.Handle, id);
+        }
+
+
+    }
+}
+}
